2e06c94c5e1fd1100d7682e6a2e75eef73d1c1c2
Muhammad Royyan Zamzami <royyanmz87@gmail.com>
2022-12-05 08:43:21 +0700

    tes

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -1,6 +1,6 @@
 <section class="content-header">
   <h1>Dashboard
-    <small>Dinas Kependudukan dan Pencatatan Sipil</small>
+    <small>smart logbook</small>
   </h1>
   <ol class="breadcrumb">
     <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
